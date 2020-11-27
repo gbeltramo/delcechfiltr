9ff8a847924bf51fd6fbb54211cf56436423f7a6
Gabriele Beltramo <gabri.beltramo@gmail.com>
2020-11-27 14:46:56 +0100

    added simplices_and_cech_param() with stacked output

--- delcechfiltr/dgms.py
+++ delcechfiltr/dgms.py
@@ -1,11 +1,16 @@
-from itertools import combinations
 import numpy as np
+from itertools import combinations
+
 from scipy.spatial.distance import euclidean
 from scipy.spatial import Delaunay
-import delcechfiltr.tri
-import gudhi
 
-def simplices_and_cech_param(points):
+import delcechfiltr.tri
+import delcechfiltr.tetra
+
+from gudhi import SimplexTree
+
+def delaunay_simplices(points):
+    """Simplices of the Euclidean Delaunay triangulation of `points`."""
     dim = len(points[0])
     if dim == 2:
         out_del = Delaunay(points)
@@ -17,21 +22,11 @@ def simplices_and_cech_param(points):
             edges_del.add((v1, v3))
             edges_del.add((v2, v3))
         edges_del = list(edges_del)
-
-        param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges_del]
-        param_tri = delcechfiltr.tri.cech_param_list(points, tri_del)
-
-        sim0 = np.array([[v, -1, -1] for v in range(len(points))])
-        sim1 = np.array([[ed[0], ed[1], -1] for ed in edges_del])
-        simplices = np.vstack([sim0, sim1, tri_del])
-        parameterization = np.hstack([np.zeros(len(points)),
-                                     param_edges,
-                                     param_tri])
-        return simplices, parameterization
+        return np.arange(len(points)), edges_del, tri_del
     elif dim == 3:
         out_del = Delaunay(points)
         tetra_del = out_del.simplices
-        tetra_del = sorted([sorted(te) for te in tetra_del])
+        tetra_del = sorted([sorted(tetra) for tetra in tetra_del])
         edges_del = set()
         tri_del = set()
         for v1, v2, v3, v4 in tetra_del:
@@ -49,6 +44,38 @@ def simplices_and_cech_param(points):
 
         edges_del = list(edges_del)
         tri_del = list(tri_del)
+        return np.arange(len(points)), edges_del, tri_del, tetra_del
+    else:
+        print("elements of `points` must be 2 or 3 dimensional")
+        return None
+
+def simplices_and_cech_param(points, stacked_output=True):
+    """Simplices of the Euclidean Delaunay triangulation of `points` and
+    their smallest encolisng ball radiuses, i.e. Cech complex parameter."""
+    dim = len(points[0])
+    if dim == 2:
+        vert, edges_del, tri_del = delaunay_simplices(points)
+        sim0 = np.array([[v, -1, -1] for v in vert])
+        sim1 = np.array([[ed[0], ed[1], -1] for ed in edges_del])
+        simplices = np.vstack([sim0, sim1, tri_del])
+
+        param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges_del]
+        param_tri = delcechfiltr.tri.cech_param_list(points, tri_del)
+        parameterization = np.hstack([np.zeros(len(points)),
+                                     param_edges,
+                                     param_tri])
+
+        if stacked_output:
+            return simplices, parameterization
+        else:
+            return vert, edges_del, tri_del, np.zeros(len(vert)), param_edges, param_tri
+    elif dim == 3:
+        vert, edges_del, tri_del, tetra_del = delaunay_simplices(points)
+        sim0 = np.array([[v, -1, -1, -1] for v in range(len(points))])
+        sim1 = np.array([[ed[0], ed[1], -1, -1] for ed in edges_del])
+        sim2 = np.array([[tri[0], tri[1], tri[2], -1]
+                         for tri in tri_del])
+        simplices = np.vstack([sim0, sim1, sim2, tetra_del])
 
         param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges_del]
         param_tri = delcechfiltr.tri.cech_param_list(points, tri_del)
@@ -58,12 +85,10 @@ def simplices_and_cech_param(points):
                                      param_tri,
                                      param_tetra])
 
-        sim0 = np.array([[v, -1, -1, -1] for v in range(len(points))])
-        sim1 = np.array([[ed[0], ed[1], -1, -1] for ed in edges_del])
-        sim2 = np.array([[tri[0], tri[1], tri[2], -1]
-                         for tri in tri_del])
-        simplices = np.vstack([sim0, sim1, sim2, tetra_del])
-        return simplices, parameterization
+        if stacked_output:
+            return simplices, parameterization
+        else:
+            return vert, edges_del, tri_del, tetra_del, np.zeros(len(vert)), param_edges, param_tri, param_tetra
     else:
         print("elements of `points` must be 2 or 3 dimensional")
         return None
@@ -72,7 +97,7 @@ def cech(points, homology_coeff_field=2,
          min_persistence=0.000000001, persistence_dim_max=True):
     edges = list(combinations(range(len(points)), 2))
     tri = list(combinations(range(len(points)), 3))
-    st = gudhi.SimplexTree()
+    st = SimplexTree()
     param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges]
     param_tri = delcechfiltr.tri.cech_param_list(points, tri)
     for i, v in enumerate(points):
@@ -93,17 +118,8 @@ def cech(points, homology_coeff_field=2,
 
 def delcech_2D(points, homology_coeff_field=2,
                min_persistence=0.000000001, persistence_dim_max=True):
-    out_del = Delaunay(points)
-    tri_del = out_del.simplices
-    tri_del = sorted([sorted(tri) for tri in tri_del])
-    edges_del = set()
-    for v1, v2, v3 in tri_del:
-        edges_del.add((v1, v2))
-        edges_del.add((v1, v3))
-        edges_del.add((v2, v3))
-    edges_del = list(edges_del)
-
-    st = gudhi.SimplexTree()
+    vert, edges_del, tri_del = delaunay_simplices(points)
+    st = SimplexTree()
     param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges_del]
     param_tri = delcechfiltr.tri.cech_param_list(points, tri_del)
     for i, v in enumerate(points):
@@ -124,28 +140,9 @@ def delcech_2D(points, homology_coeff_field=2,
 
 def delcech_3D(points, homology_coeff_field=2,
                min_persistence=0.000000001, persistence_dim_max=True):
-    out_del = Delaunay(points)
-    tetra_del = out_del.simplices
-    tetra_del = sorted([sorted(te) for te in tetra_del])
-    edges_del = set()
-    tri_del = set()
-    for v1, v2, v3, v4 in tetra_del:
-        edges_del.add((v1, v2))
-        edges_del.add((v1, v3))
-        edges_del.add((v1, v4))
-        edges_del.add((v2, v3))
-        edges_del.add((v2, v4))
-        edges_del.add((v3, v4))
 
-        tri_del.add((v1, v2, v3))
-        tri_del.add((v1, v2, v4))
-        tri_del.add((v1, v3, v4))
-        tri_del.add((v2, v3, v4))
-
-    edges_del = list(edges_del)
-    tri_del = list(tri_del)
-
-    st = gudhi.SimplexTree()
+    vert, edges_del, tri_del, tetra_del = delaunay_simplices(points)
+    st = SimplexTree()
     param_edges = [euclidean(points[i], points[j]) / 2.0  for (i,j) in edges_del]
     param_tri = delcechfiltr.tri.cech_param_list(points, tri_del)
     for i, v in enumerate(points):
